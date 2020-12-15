d50282d1b9a53e1b6630feb571efba3544920da2
GedoRinneRebirth <59303347+GedoRinneRebirth@users.noreply.github.com>
2020-12-15 20:04:55 +0530

    Added input_page.dart by abstracting main.dart

---
+++ lib/input_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI Calculator'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[Expanded(child: null), Expanded(child: null)],
+            ),
+          ),
+          Expanded(child: null),
+          Expanded(child: null),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'input_page.dart';
 
 void main() {
   runApp(BMICalculator());
@@ -8,34 +9,11 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primaryColor: Colors.red,
-        accentColor: Colors.purple,
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xFF0A0e21),
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
       ),
       home: InputPage(),
-    );
-  }
-}
-
-class InputPage extends StatefulWidget {
-  @override
-  _InputPageState createState() => _InputPageState();
-}
-
-class _InputPageState extends State<InputPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI Calculator'),
-      ),
-      body: Center(
-        child: Text('Body Text'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {},
-      ),
     );
   }
 }
